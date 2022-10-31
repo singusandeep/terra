b856a08db5694df53ffe8ec6407f31462a581044
singusandeep <106309029+singusandeep@users.noreply.github.com>
2022-10-31 11:17:46 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,11 @@
 variable "aws_access_key" {
   type        = string
   description = "AWS Access Key"
-  sensitive   = true
 }
 
 variable "aws_secret_key" {
   type        = string
   description = "AWS Secret Key"
-  sensitive   = true
 }
 
 variable "aws_region" {
