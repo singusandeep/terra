b142f4b71d841b2a4dff20699f22913883d2c8f2
singusandeep <106309029+singusandeep@users.noreply.github.com>
2022-10-31 12:43:05 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -71,33 +71,7 @@ resource "aws_route_table_association" "rta-subnet2" {
   route_table_id = aws_route_table.rtb.id
 }
 
-# SECURITY GROUPS #
-# ALB Security Group
-resource "aws_security_group" "alb_sg" {
-  name   = "nginx_alb_sg"
-  vpc_id = aws_vpc.vpc.id
 
-  
-
-  #Allow HTTP from anywhere
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  #allow all outbound
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = local.common_tags
-
-}
 
 # Nginx security group 
 resource "aws_security_group" "nginx-sg" {
