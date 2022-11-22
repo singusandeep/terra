1dd3dbecba10aaabdb5fbe07389bf69a9e91fb5d
singusandeep <106309029+singusandeep@users.noreply.github.com>
2022-11-22 12:39:03 +0530

    Update instances.tf

--- instances.tf
+++ instances.tf
@@ -29,7 +29,7 @@ sudo amazon-linux-extras install -y nginx1
 sudo service nginx start
 sudo rm /usr/share/nginx/html/index.html
 echo '<html><head><title>Taco Team Server 1</title></head><body style=\"background-color:#1F778D\"><p style=\"text-align: center;\"><span style=\"color:#FFFFFF;\"><span style=\"font-size:28px;\">You did it! Have a &#127790;</span></span></p></body></html>' | sudo tee /usr/share/nginx/html/index.html
-EOF
+
 
   tags = local.common_tags
 
@@ -55,7 +55,7 @@ sudo amazon-linux-extras install -y nginx1
 sudo service nginx start
 sudo rm /usr/share/nginx/html/index.html
 echo '<html><head><title>Taco Team Server 2</title></head><body style=\"background-color:#1F778D\"><p style=\"text-align: center;\"><span style=\"color:#FFFFFF;\"><span style=\"font-size:28px;\">You did it! Have a &#127790;</span></span></p></body></html>' | sudo tee /usr/share/nginx/html/index.html
-EOF
+
 
   tags = local.common_tags
 
